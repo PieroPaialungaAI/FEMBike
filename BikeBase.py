f59123be493143f376166f8d8ed38f80c5ae1387
reedn4 <64878089+reedn4@users.noreply.github.com>
2022-04-13 16:42:45 -0400

    Static solve fix

--- BikeBase.py
+++ BikeBase.py
@@ -31,7 +31,10 @@ def frameForm(propFile):
         nodeDF.loc[ind2] = temp2
         for node in range(eNum):
             if node == 0:
-                elemDF.loc[eInd] = [ind1, nInd]+conLst
+                if node == eNum - 1:
+                    elemDF.loc[eInd] = [ind1, ind2] + conLst
+                else:
+                    elemDF.loc[eInd] = [ind1, nInd]+conLst
                 eInd += 1
             else:
                 if node == eNum - 1:
@@ -85,9 +88,12 @@ def kMat(A, E, G, a, Iy, Iz, J):
     return k
 
 
-def transM(locUV, globC):
+def transM(locUV, globC, dim2=False):
     # Find local y and z directions
-    v2 = [locUV[num]+num for num in range(3)]
+    if dim2:
+        v2 = locUV+[1, 2, 0]
+    else:
+        v2 = locUV+[1, 2, 3]
     v1 = np.cross(locUV, v2)
     v2 = np.cross(locUV, v1)
     # Convert to unit vector
@@ -95,11 +101,17 @@ def transM(locUV, globC):
     v2 = v2 / np.sqrt(np.dot(v2, v2))
     locUV = locUV / np.sqrt(np.dot(locUV, locUV))
     # Define local and global coordinate systems
-    locC = np.array([locUV, v1, v2])
+    locC = np.array([locUV, v2, v1])
     tVal = np.zeros((3, 3))
     for gC in range(3):
         for lC in range(3):
             tVal[lC, gC] = np.dot(locC[lC, :], globC[gC, :])
+    if dim2:
+        tVal[:, 2] = [0, 0, 1]
+        tVal[2, :] = [0, 0, 1]
+        tVal[1, :] = np.abs(tVal[1, :])
+        tVal[:, 1] = np.abs(tVal[:, 1])
+    print(tVal)
     tMat = np.zeros((12, 12))
     tMat[0:3, 0:3] = tVal
     tMat[3:6, 3:6] = tVal
@@ -114,7 +126,7 @@ def findClose(pt, nodeDF):
     return ind, nodeDF.iloc[ind]
 
 
-def glob(nodeDF, elemDF, globC):
+def glob(nodeDF, elemDF, globC, dim2=False):
     nNum = len(nodeDF)
     massG = np.zeros((nNum*6, nNum*6), dtype=float)
     kG = np.zeros((nNum*6, nNum*6), dtype=float)
@@ -123,7 +135,7 @@ def glob(nodeDF, elemDF, globC):
         tempM = massMat(row['rho'], row['A'], row['a'], row['J'])
         tempK = kMat(row['A'], row['E'], row['G'], row['a'], row['Iy'], row['Iz'], row['J'])
         # Coordinate transformation
-        tMat = transM(row[['xV', 'yV', 'zV']].to_numpy(), globC)
+        tMat = transM(row[['xV', 'yV', 'zV']].to_numpy(), globC, dim2)
         tMatT = np.transpose(tMat)
         tempM = np.matmul(np.matmul(tMatT, tempM), tMat)
         tempK = np.matmul(np.matmul(tMatT, tempK), tMat)
@@ -152,43 +164,76 @@ def glob(nodeDF, elemDF, globC):
         kG[6 * node2:6 * (node2 + 1), 6 * node2:6 * (node2 + 1)] += tempK[6:, 6:]
         kG[6 * node1:6 * (node1 + 1), 6 * node2:6 * (node2 + 1)] += tempK[:6, 6:]
         kG[6 * node2:6 * (node2 + 1), 6 * node1:6 * (node1 + 1)] += tempK[6:, :6]
-    return massG, kG
+        # pd.DataFrame(kG).to_csv("Global3D.csv")
+    if dim2:
+        delInd = np.tile([2, 3, 4], nNum) + 6 * np.repeat(np.arange(nNum), 3)
+        kG_R = np.delete(kG, delInd, axis=0)
+        kG_R = np.delete(kG_R, delInd, axis=1)
+        massG_R = np.delete(massG, delInd, axis=0)
+        massG_R = np.delete(massG_R, delInd, axis=1)
+        # pd.DataFrame(kG_R).to_csv("Global2D.csv")
+        return massG_R, kG_R
+
+    else:
+        return massG, kG
 
 
-def appBound(boundFile, nodeDF):
+def appBound(boundFile, nodeDF, dim2 = False):
     boundDF = pd.read_csv(boundFile, header=0, skiprows=[1])
     indLst = []
     for index, row in boundDF.iterrows():
         # Locate closest point
         ind, _ = findClose(row[['X', 'Y', 'Z']].to_numpy(), nodeDF)
-        # Define fixed degrees of freedom
-        lInd = np.where(row[['DOF 1', 'DOF 2', 'DOF 3', 'DOF 4', 'DOF 5', 'DOF 6']].to_numpy())[0].tolist()
-        if len(lInd) > 0:
-            indLst.extend(6*ind+lInd)
+        if dim2:
+            # Define fixed degrees of freedom
+            lInd = np.where(row[['DOF 1', 'DOF 2', 'DOF 6']].to_numpy())[0].tolist()
+            if len(lInd) > 0:
+                indLst.extend(3*ind+lInd)
+        else:
+            # Define fixed degrees of freedom
+            lInd = np.where(row[['DOF 1', 'DOF 2', 'DOF 3', 'DOF 4', 'DOF 5', 'DOF 6']].to_numpy())[0].tolist()
+            if len(lInd) > 0:
+                indLst.extend(6 * ind + lInd)
     indLst.sort()
     return indLst
 
 
 def lamDef(mag, scale):
-    return lambda t: mag*scale*t
+    return lambda t: mag*scale*np.sin(t)
 
 
-def forceVect(forceFile, nodeDF):
+def forceVect(forceFile, nodeDF, dim2 = False):
     forceDF = pd.read_csv(forceFile, header=0, skiprows=[1])
-    staticFV = np.zeros((6*len(nodeDF),), dtype=float)
     dynamicFunc = {}
-    for index, row in forceDF.iterrows():
-        # Calculated unit direction vector
-        temp = row[['xV', 'yV', 'zV']].to_numpy()
-        dV = np.sqrt(np.dot(temp, temp))
-        temp = temp/dV
-        # Locate closest point
-        ind, _ = findClose(row[['X', 'Y', 'Z']].to_numpy(), nodeDF)
-        if row['freq'] > 0:
-            for dirI in range(3):
-                dynamicFunc[6*ind+dirI] = lamDef(row['mag'], temp[dirI])
-        else:
-            staticFV[6*ind+[0, 1, 2]] = row['mag']*temp
+    if dim2:
+        staticFV = np.zeros((3 * len(nodeDF),), dtype=float)
+        for index, row in forceDF.iterrows():
+            # Calculated unit direction vector
+            temp = row[['xV', 'yV', 'zV']].to_numpy()
+            dV = np.sqrt(np.dot(temp, temp))
+            temp = temp / dV
+            # Locate closest point
+            ind, _ = findClose(row[['X', 'Y', 'Z']].to_numpy(), nodeDF)
+            if row['freq'] > 0:
+                for dirI in range(2):
+                    dynamicFunc[3 * ind + dirI] = lamDef(row['mag'], temp[dirI])
+            else:
+                staticFV[3 * ind + [0, 1]] = row['mag'] * temp[:2]
+    else:
+        staticFV = np.zeros((6 * len(nodeDF),), dtype=float)
+        for index, row in forceDF.iterrows():
+            # Calculated unit direction vector
+            temp = row[['xV', 'yV', 'zV']].to_numpy()
+            dV = np.sqrt(np.dot(temp, temp))
+            temp = temp/dV
+            # Locate closest point
+            ind, _ = findClose(row[['X', 'Y', 'Z']].to_numpy(), nodeDF)
+            if row['freq'] > 0:
+                for dirI in range(3):
+                    dynamicFunc[6 * ind + dirI] = lamDef(row['mag'], temp[dirI])
+            else:
+                staticFV[6 * ind + [0, 1, 2]] = row['mag'] * temp
+
     return staticFV, dynamicFunc
 
 
@@ -196,54 +241,86 @@ def staticSolve(kM, forceV, boundInd):
     # Apply boundary conditions
     kM_R = np.delete(kM, boundInd, axis=0)
     kM_R = np.delete(kM_R, boundInd, axis=1)
+    # print(kM_R)
+    print(np.shape(kM_R))
     fV_R = np.delete(forceV, boundInd)
+    print(np.shape(fV_R))
     # Solve for nodal displacement
     # lu, piv = la.lu_factor(kM_R)
     # nDis = la.lu_solve((lu, piv), fV_R)
-    nDis = la.solve(kM_R, fV_R, assume_a='sym')
-    nDis = np.insert(nDis, boundInd, np.zeros(len(boundInd),))
+    nDis_R = la.solve(kM_R, fV_R, assume_a='sym')
+    nDis = np.zeros(np.shape(forceV))
+    nDis[boundInd] = np.zeros(len(boundInd))
+    mapInd = np.delete(np.arange(len(forceV)), boundInd)
+    nDis[mapInd] = nDis_R
     nFor = np.matmul(kM, nDis)
     return nDis, nFor
 
 
+# Verification (HW 5)
 pFolder = r"C:\Users\Natalie\OneDrive - University of Cincinnati\Documents\UCFiles\Spring2022\AEEM7052\Final Project"
-frameFile = pFolder + r"\FramePropertiesBase.csv"
-boundFile = pFolder + r"\boundaryCondBase.csv"
-forceFile = [pFolder + r"\appForceBase_" + s + ".csv" for s in ["S1", "S2", "D1"]]
-# forceFile = [pFolder + r"\appForce_simp.csv"]
+frameFile = pFolder + r"\FramePropertiesVer.csv"
+boundFile = pFolder + r"\boundaryCondVer.csv"
+forceFile = pFolder + r"\appForceVer.csv"
 
 globC = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
 nodeDF, elemDF = frameForm(frameFile)
-# nodeDF.to_csv("nodes.csv")
-# elemDF.to_csv("elements.csv")
-mM, kM = glob(nodeDF, elemDF, globC)
+nodeDF.to_csv("nodesVer.csv")
+elemDF.to_csv("elementsVer.csv")
+mM, kM = glob(nodeDF, elemDF, globC, True)
+# print(kM)
+print(np.shape(kM))
 
-# print("Mass Matrix Symmetry")
-# print(np.allclose(mM, mM.T))
-# print("Stiffness Matrix Symmetry")
-# print(np.allclose(kM, kM.T))
-# print("Stiffness Matrix Row Sum:")
-# print(np.sum(kM, axis=0))
-# print("Stiffness Matrix Column Sum:")
-# print(np.sum(kM, axis=1))
-
-boundInd = appBound(boundFile, nodeDF)
+boundInd = appBound(boundFile, nodeDF, True)
 print(boundInd)
 
-solI = 1
-for file in forceFile:
-    staticFV, dynamicFunc = forceVect(file, nodeDF)
-    if bool(dynamicFunc):
-        print("Dynamic Case")
-    else:
-        print("Static Case")
-        nDis, nFor = staticSolve(kM, staticFV, boundInd)
-        nd_df = pd.DataFrame(data=np.array_split(nDis, len(nodeDF)), columns=["DOF 1 Displacement", "DOF 2 Displacement", "DOF 3 Displacement", "DOF 4 Displacement", "DOF 5 Displacement", "DOF 6 Displacement"])
-        nf_df = pd.DataFrame(data=np.array_split(nFor, len(nodeDF)), columns=["DOF 1 Force", "DOF 2 Force", "DOF 3 Force", "DOF 4 Force", "DOF 5 Force", "DOF 6 Force"])
-        strT = "staticRes"+str(solI)+".csv"
-        pd.concat([nodeDF, nd_df, nf_df], axis=1).to_csv(strT)
-        solI += 1
-        print(nDis[:6])
-        print(nFor[:6])
+staticFV, dynamicFunc = forceVect(forceFile, nodeDF, True)
+# print(staticFV)
+print(np.shape(staticFV))
+nDis, nFor = staticSolve(kM, staticFV, boundInd)
+nd_df = pd.DataFrame(data=np.array_split(nDis, len(nodeDF)), columns=["DOF 1 Displacement", "DOF 2 Displacement", "DOF 6 Displacement"])
+nf_df = pd.DataFrame(data=np.array_split(nFor, len(nodeDF)), columns=["DOF 1 Force", "DOF 2 Force", "DOF 6 Force"])
+pd.concat([nodeDF, nd_df, nf_df], axis=1).to_csv("Verification.csv")
 
+# # Bike Problem
+# pFolder = r"C:\Users\Natalie\OneDrive - University of Cincinnati\Documents\UCFiles\Spring2022\AEEM7052\Final Project"
+# frameFile = pFolder + r"\FramePropertiesBase.csv"
+# boundFile = pFolder + r"\boundaryCondBase.csv"
+# forceFile = [pFolder + r"\appForceBase_" + s + ".csv" for s in ["S1", "S2", "D1"]]
+#
+# globC = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+#
+# nodeDF, elemDF = frameForm(frameFile)
+# # nodeDF.to_csv("nodes.csv")
+# # elemDF.to_csv("elements.csv")
+# mM, kM = glob(nodeDF, elemDF, globC)
+#
+# # print("Mass Matrix Symmetry")
+# # print(np.allclose(mM, mM.T))
+# # print("Stiffness Matrix Symmetry")
+# # print(np.allclose(kM, kM.T))
+# # print("Stiffness Matrix Row Sum:")
+# # print(np.sum(kM, axis=0))
+# # print("Stiffness Matrix Column Sum:")
+# # print(np.sum(kM, axis=1))
+#
+# boundInd = appBound(boundFile, nodeDF)
+# print(boundInd)
+#
+# solI = 1
+# for file in forceFile:
+#     staticFV, dynamicFunc = forceVect(file, nodeDF)
+#     if bool(dynamicFunc):
+#         print("Dynamic Case")
+#     else:
+#         print("Static Case")
+#         nDis, nFor = staticSolve(kM, staticFV, boundInd)
+#         nd_df = pd.DataFrame(data=np.array_split(nDis, len(nodeDF)), columns=["DOF 1 Displacement", "DOF 2 Displacement", "DOF 3 Displacement", "DOF 4 Displacement", "DOF 5 Displacement", "DOF 6 Displacement"])
+#         nf_df = pd.DataFrame(data=np.array_split(nFor, len(nodeDF)), columns=["DOF 1 Force", "DOF 2 Force", "DOF 3 Force", "DOF 4 Force", "DOF 5 Force", "DOF 6 Force"])
+#         strT = "staticRes"+str(solI)+".csv"
+#         pd.concat([nodeDF, nd_df, nf_df], axis=1).to_csv(strT)
+#         solI += 1
+#         print(nDis[:6])
+#         print(nFor[:6])
+#
